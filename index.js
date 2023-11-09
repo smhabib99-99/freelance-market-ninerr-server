123b9f7c0fc3531a23810745da41abb27340161b
smhabib99-99 <habibaust34@gmail.com>
2023-11-09 14:29:08 +0600

    post data to server from client side is successful

--- index.js
+++ index.js
@@ -31,6 +31,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+
+    app.post('/addJobs', async(req,res)=>{
+        const newJobs = req.body;
+        console.log(newJobs);
+    })
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
