87801feda27e6bd2ae8090a6c13facd352fbc34b
smhabib99-99 <habibaust34@gmail.com>
2023-11-09 09:17:17 +0600

    Server side basic setup test is successful

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send('Ninerr is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Ninerr server is running on port ${port}`)
+})
